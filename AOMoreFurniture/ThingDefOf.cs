35939165005c0c2b38946726c3babdaeac8e7157
Atlas <16dayaaron@colchsfc.ac.uk>
2019-04-10 19:52:59 +0100

    Radios Completed
    Radios are a source of good mood, although very weak and with limited range.

---
+++ AOMoreFurniture/ThingDefOf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace AOMoreFurniture
+{
+    [DefOf]
+    public static class ThingDefOf
+    {
+        static ThingDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOf));
+        }
+
+        
+        public static ThingDef Radio_Industrial;
+
+        public static ThingDef Radio_Spacer;
+    }
+
+}
---
+++ AOMoreFurniture/ThoughtWorker_RadioBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace AOMoreFurniture
+{
+    class ThoughtWorker_RadioBase : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            bool radio_result = false;
+
+            if (!p.Spawned)
+            {
+                return false;
+            }
+            List<Thing> listtwo = p.Map.listerThings.ThingsOfDef(ThingDefOf.Radio_Spacer);
+            for (int i = 0; i < listtwo.Count; i++)
+            {
+                CompPowerTrader compPowerTrader = listtwo[i].TryGetComp<CompPowerTrader>();
+                if (compPowerTrader == null || compPowerTrader.PowerOn)
+                {
+                    if (p.Position.InHorDistOf(listtwo[i].Position, 8f))
+                    {
+                        radio_result = true;
+                        return ThoughtState.ActiveAtStage(1);
+                    }
+                }
+            }
+            List<Thing> list = p.Map.listerThings.ThingsOfDef(ThingDefOf.Radio_Industrial);
+            for (int i = 0; i < list.Count; i++)
+            {
+                CompPowerTrader compPowerTrader = list[i].TryGetComp<CompPowerTrader>();
+                if (compPowerTrader == null || compPowerTrader.PowerOn && radio_result == false)
+                {
+                    if (p.Position.InHorDistOf(list[i].Position, 5f))
+                    {
+                        return ThoughtState.ActiveAtStage(0);
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
