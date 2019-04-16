24aae8c95a5d7e37753823ac022c1f4973eee4f7
Atlas <16dayaaron@colchsfc.ac.uk>
2019-04-17 00:37:20 +0100

    Bins Finished

---
+++ AOMoreFurniture/CompBinClean.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace AOMoreFurniture
+{
+    public class CompBinClean : ThingComp
+    {
+        public CompProperties_BinClean Cleanprops
+        {
+            get
+            {
+                return this.props as CompProperties_BinClean;
+            }
+        }
+
+        public override void CompTick()
+        {
+            TicksCounted++;
+
+            Log.Message(Convert.ToString(TicksCounted));
+
+            if (TicksCounted == Cleanprops.TimerInTicks)
+            {
+                TicksCounted = 0;
+                Map BuildingMap = parent.Map;
+
+                List<Thing> Filthlist = BuildingMap.listerFilthInHomeArea.FilthInHomeArea;
+                for (int i = 0; i < Filthlist.Count; i++)
+                {
+                    if (Filthlist[i].Position.InHorDistOf(parent.Position, Cleanprops.Radius))
+                    {
+                        Filthlist[i].Destroy();
+                        break;
+                    }
+                }
+            }
+            base.CompTick();
+        }
+
+        public int TicksCounted = 0;
+    }
+}
---
+++ AOMoreFurniture/CompProperties_BinClean.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace AOMoreFurniture 
+{
+    public class CompProperties_BinClean : CompProperties
+    {
+        public CompProperties_BinClean()
+        {
+            this.compClass = typeof(CompBinClean);
+        }
+
+        public int TimerInTicks;
+
+        public float Radius;
+    }
+}
